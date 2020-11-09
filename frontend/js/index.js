f1f1616a355f9a63588b25d356cb4eb1cdba64f3
Juliette McCavana <jmccavana@aol.com>
2020-11-09 18:30:38 +0000

    test d'utilisation const ajax

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,9 @@
 let container = document.getElementById("cam_list") // Création variable container qui correspond à la div "cam_list" du fichier index.html
 let url = "http://localhost:3000/api/cameras"
+
+// ci dessous teste d'utilisation
+const ajax = new Ajax();
+ajax.get('http://localhost:3000/api/cameras', (response) => {
+
+console.log(response);
+});
