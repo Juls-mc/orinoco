3495a7c24250292e18923fc3ab5db99fcd282ce0
Juliette McCavana <jmccavana@aol.com>
2020-11-16 17:19:49 +0000

    récupération données API avec fetch, utilisation du .then et .catch

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -11,11 +11,11 @@ ajax.get('http://localhost:3000/api/cameras', (response) => {
 
 
 
-// Récupération données de l'API avec fetch
+// Récupère les données de l'API avec fetch
 fetch(url)
     .then(cameras => cameras.json()) // Retourner une promesse
     .then(cameras => {
-        // récupère chaque élément du tableau
+        // récupère chaque élément du tableau avec Array.prototype.forEach
         cameras.forEach(({
             _id,
             name,
@@ -23,7 +23,7 @@ fetch(url)
             description,
             imageUrl,
         }) => {
-            // Création des nouveaux éléments avec createElement et createTextNode
+            // Créer nouveaux éléments avec createElement et createTextNode
             let div = document.createElement("div")
             let img = document.createElement("img")
             let h3 = document.createElement("h3")
@@ -35,17 +35,17 @@ fetch(url)
             let elePrice = document.createTextNode(price / 100 + " €")
             let eleDescription = document.createTextNode(description)
 
-            // Etape 2: Définition des informations des éléments
+            // Définir les informations des éléments
             a.href = 'product.html?id=' + _id;
             a.textContent = "Voir ce produit";
             img.src = imageUrl;
 
-            // Etape 3: Insertion des nouveaux éléments dans le DOM grâce à appendChild
+            // Insérer des nouveaux éléments dans le DOM grace à appendChild
             container.appendChild(div)
             div.appendChild(img)
             div.appendChild(h3)
-            div.appendChild(h4) /
-                div.appendChild(p)
+            div.appendChild(h4)
+            div.appendChild(p)
             div.appendChild(a)
             h3.appendChild(eleName)
             h4.appendChild(elePrice)
