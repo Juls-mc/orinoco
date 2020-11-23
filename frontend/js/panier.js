3b8449bc9383ab92bb34d1b30dc8fc354d7efdb1
Juliette McCavana <jmccavana@aol.com>
2020-11-23 16:21:26 +0000

    beautify code js

--- frontend/js/panier.js
+++ frontend/js/panier.js
@@ -1,197 +1,57 @@
-(function () {
-  'use strict'
-
-  window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation')
-
-    // Loop over them and prevent submission
-    Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-  }, false)
-}())
-
-
-
-
 // Fonction affichage produit au panier
 function affichagePanier() {
-  //je récupére mon produit dans local storage "panier"
-  let panier = JSON.parse(localStorage.getItem("panier"))
-  let prixTotal = JSON.parse(localStorage.getItem("prixTotal"))
-  let prixPanier = document.getElementById('affichageTotal')
-  prixPanier.className = "list-group-item d-flex justify-content-between"
+    //je récupére mon produit dans local storage "panier"
+    let panier = JSON.parse(localStorage.getItem("panier"))
+    let prixTotal = JSON.parse(localStorage.getItem("prixTotal"))
+    let prixPanier = document.getElementById('affichageTotal')
 
-  let tableauPanier = document.getElementById("afficheProduitPanier")
 
-  // affichage du prix total du panier si le panier:
-  if (prixTotal != null) {
-      prixPanier.textContent = 'Le montant de votre commande est de : ' + prixTotal +  ' €';
-      prixPanier.id = 'prixTotal';
-      let div = document.createElement("div")
-      div.textContent = "Le panier est vide!"
-      afficheProduitPanier.appendChild(div)
-  } else  {
-      prixPanier.textContent = 'Le montant de votre commande est de : 0 €';
-  }
+    let tableauPanier = document.getElementById("afficheProduitPanier")
 
-  // si il n'y a rien dans le panier, affiche "Le panier est vide!"
-  if (panier == null) {
-      let div = document.createElement("div")
-      div.textContent = "Le panier est vide!"
-      afficheProduitPanier.appendChild(div)
-      console.log("Le panier est vide!!!!")
-  } else {
-      //s'il y a qq chose, créé un tableau contenant chaque produit
-      tableauPanier.innerHTML = ''
-      Object.values(panier).map((teddies) => {
-          let tr = document.createElement("tr")
-          afficheProduitPanier.appendChild(tr)
+    // affichage du prix total du panier si le panier:
+    if (prixTotal != null) {
+        prixPanier.textContent = 'Le montant total de votre commande est de : ' + prixTotal + ' €';
+        prixPanier.id = 'prixTotal';
+        let div = document.createElement("div")
+        div.textContent = "Votre panier est vide!"
+        afficheProduitPanier.appendChild(div)
+    } else {
+        prixPanier.textContent = 'Le montant de votre commande est de : 0 €';
+    }
 
-              let name = document.createElement("td")
-              name.textContent = teddies.name
-              tr.appendChild(name)
+    // si il n'y a rien dans le panier, affiche "Le panier est vide!"
+    if (panier == null) {
+        let div = document.createElement("div")
+        div.textContent = "Votre panier est vide!"
+        afficheProduitPanier.appendChild(div)
+        console.log("Le panier est vide")
+    } else {
+        //s'il y a qq chose, créé un tableau contenant chaque produit
+        tableauPanier.innerHTML = ''
+        Object.values(panier).map((cameras) => {
+            let tr = document.createElement("tr")
+            afficheProduitPanier.appendChild(tr)
 
-              let couleur = document.createElement("td")
-              couleur.textContent = teddies.colors
-              tr.appendChild(couleur)
+            let name = document.createElement("td")
+            name.textContent = cameras.name
+            tr.appendChild(name)
 
-              let description = document.createElement("td")
-              description.textContent = teddies.description
-              tr.appendChild(description)
+            let lenses = document.createElement("td")
+            lenses.textContent = cameras.lenses
+            tr.appendChild(lenses)
 
-              let quantite = document.createElement("td")
-              quantite.textContent = teddies.quantity
-              tr.appendChild(quantite)
 
-              let prix = document.createElement("td")
-              prix.textContent = teddies.price/100  + "€"
-              tr.appendChild(prix)
+            let quantite = document.createElement("td")
+            quantite.textContent = cameras.quantity
+            tr.appendChild(quantite)
+
+            let prix = document.createElement("td")
+            prix.textContent = cameras.price / 100 + "€"
+            tr.appendChild(prix)
 
             console.log("Voici le panier :")
             console.log(panier)
-      })
-  }
+        })
+    }
 }
 affichagePanier() // appel de la fonction
-
-
-
-
-
-//création des variables d'informations client
-let orderButton = document.querySelector(".order-submit");
-let validationButton = document.querySelector(".validation");
-let firstName = document.querySelector("#firstName");
-let lastName = document.querySelector("#lastName");
-let eMail = document.querySelector("#inputEmail");
-let telephoneNumber = document.querySelector("#telephoneNumber");
-let address = document.querySelector("#inputAddress");
-let city = document.querySelector("#inputCity");
-let zip = document.querySelector("#inputZip");
-
-
-// création de l'objet général client
-function Client (firstName, lastName, eMail, telephoneNumber, address, city, zip){
-      (this.firstName = firstName),
-      (this.lastName = lastName),
-      (this.eMail = eMail),
-      (this.telephoneNumber = telephoneNumber),
-      (this.address = address),
-      (this.city = city),
-      (this.zip = zip);
-  }
-
-//création d'un tableau avec les articles commandés
-let panier = JSON.parse(localStorage.getItem("panier"))
-let listIdProduct = [];
-for (let i = 0; i < panier.length; i++) {
-  listIdProduct.push(panier[i].iD);
-}
-localStorage.setItem("products", JSON.stringify(listIdProduct ));
-listIdProduct  = localStorage.getItem("products");
-listIdProduct  = JSON.parse(listIdProduct );
-
-
-// création du gestionnaire d'événement en cas de clic sur le bouton submit
-if (panier == null){
-  alert("Votre panier est vide vous ne pouvez pas passer commande.")
-} else {
-orderButton.classList.remove("disabled");
-}
-
-// fonction qui permet de valider chaque input
-function validationInput (){
-  let regexEmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/
-  if (firstName.value.length === 0){
-    alert("Merci de renseigner votre prénom.")
-  } else if (lastName.value.length === 0){
-    alert("Merci de renseigner votre nom")
-  } else if (eMail.value.length === 0 || !regexEmail.test(eMail.value)) {
-      alert("Merci de renseigner votre adresse mail valide")
-      eMail.style.borderColor = "red"
-  } else if (address.value.length === 0){
-    alert("Merci de renseigner une adresse.")
-  } else if (telephoneNumber.value.length === 0){
-    alert("Merci de renseigner un numéro valide.")
-  } else if (city.value.length === 0){
-    alert("Merci de renseigner votre ville.")
-  } else if (zip.value.length === 0){
-    alert("Merci de renseigner un code postal valide.")
-  } else {
-    alert("Vos informations ont bien été enregistrées. Vous pouvez à présent valider votre commande.");
-    validationButton.classList.remove("disabled");
-    send () // si tout est ok on créé un nouveau client et on envoie au serveur
-  }
-}
-
-// Gestionnaire événément en cas de clic sur confirmer
-  orderButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  validationInput() // appel de la fonction on vérfie les inputs
-  });
-
-  //création fonction send
-  function send() {
-    // Création nouveau client
-    let newClient = new Client(
-      firstName.value,
-      lastName.value,
-      eMail.value,
-      telephoneNumber.value,
-      address.value,
-      city.value,
-      zip.value
-    );
-  // POST API
-  fetch("http://localhost:3000/api/cameras/order", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      contact: {
-        firstName: newClient.firstName,
-        lastName: newClient.lastName,
-        address: newClient.address,
-        city: newClient.city,
-        email: newClient.eMail,
-      },
-      products: listIdProduct,
-    }),
-  })
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then((data) => {
-    localStorage.setItem("orderInfos", JSON.stringify(data));
-  })
-  .catch((error) => console.log("erreur de type : ", error));
-  }
