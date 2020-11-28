7cb840531932a4a3b6727ded4c32229b838cfaf0
Juliette McCavana <jmccavana@aol.com>
2020-11-28 15:58:08 +0000

    affichage données (prix, nom client et #commande) dans page confirmation

---
+++ frontend/js/confirmation.js
@@ -0,0 +1,21 @@
+//création des variables pour l'affichage des données
+let affichagePrixTotal = document.querySelector(".totalPrice span");
+let affichageID = document.querySelector(".commandeID span");
+let orderInfo = localStorage.getItem("orderInfos");
+orderInfo = JSON.parse(localStorage.orderInfos);
+let prixTotal = JSON.parse(localStorage.getItem("prixTotal"))
+let orderId = orderInfo.orderId;
+let affichageNom = document.querySelector(".name");
+let nameGet = localStorage.getItem("orderInfos");
+nameGet = JSON.parse(nameGet);
+
+affichagePrixTotal.textContent = prixTotal; //affichage du prix total
+affichageID.textContent = orderId; //affichage de l'id de commande
+affichageNom.textContent = `${nameGet.contact.firstName} ${nameGet.contact.lastName}`; //affichage du nom de la personne
+
+
+
+//reset le panier au départ de la page
+window.addEventListener("unload", function () {
+    localStorage.clear();
+});
